cb47a2c4aafc5198c1f7954f381772ccbc55dfd4
user <domandarya@gmail.com>
2026-03-02 22:31:25 +0500

    Проверьте что новые посты создаются и отображаются на главной страницы.

--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -3,16 +3,24 @@ package com.example.demo.service;
 import com.example.demo.model.Post;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Service
 public class PostService {
+    private List<Post> posts;
+    {
+        posts = new ArrayList<>();
+        posts.add(new Post("Первый пост :)", new Date()));
+        posts.add(new Post("Второй пост :(", new Date()));
+        posts.add(new Post("Третий пост :/", new Date()));
+    }
     public List<Post> listAllPosts() {
-        return List.of(
-                new Post("Первый пост :)", new Date()),
-                new Post("Второй пост :(", new Date()),
-                new Post("Третий пост :/", new Date())
-        );
+        return posts;
+    }
+
+    public void create(String text) {
+        posts.add(new Post(text, new Date()));
     }
 }
